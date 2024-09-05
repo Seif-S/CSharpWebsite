027feafc1c346c1973a6da8f71679a3e21815bac
Seif <Seif-diya@hotmail.com>
2024-09-05 10:48:59 +0200

    Blazer website

---
+++ WebBlazorApp/ChatHub/Chat.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebBlazorApp.ChatHub
+{
+    public class Chat : Hub
+    {
+        public async Task SendData(string username, string Post) => await Clients.All.SendAsync("RecievedPost", username, Post);
+    }
+}
